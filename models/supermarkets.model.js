a733c03c49d63989dd166f41f1cbaf4c3568671e
iedenis <denis.ievlev@gmail.com>
2018-10-22 02:54:40 +0300

    cleaning the code & radius 50km added

--- models/supermarkets.model.js
+++ models/supermarkets.model.js
@@ -23,26 +23,25 @@ SupermarketModel.getAll = () => {
 
 SupermarketModel.getSupermarketsByDistance = async (lat, lon, radius) => {
     let supermarkets = await SupermarketModel.find({});
-    // console.log("ALL SUPERMARKETS: "+supermarkets);
-    var superM= {
+    var superM = {
         name: String,
-            distance: Number
+        distance: Number
     }
-    let supers =[superM];
+    let supers = [superM];
     for (let i = 0; i < supermarkets.length; i++) {
-        let dist = calcCrow(supermarkets[i].coordinates.latitude, supermarkets[i].coordinates.longitude, lat, lon);
-
-        supers[i]={
-            name: supermarkets[i].name,
-            distance: dist
+        let dist = calcDistance(supermarkets[i].coordinates.latitude, supermarkets[i].coordinates.longitude, lat, lon);
+        if (dist < 50) {
+            supers[i] = {
+                name: supermarkets[i].name,
+                distance: dist
+            }
         }
     }
-    
-    // console.log("distance: " + dist);
-    //console.log("calculation check: "+calcCrow(32.174094, 34.837425, 32.0596847, 34.8149172));
     return supers.sort(SupermarketModel.compare);
+
 }
-SupermarketModel.compare = (a, b)=>{
+
+SupermarketModel.compare = (a, b) => {
     return a.distance - b.distance;
 }
 SupermarketModel.addCar = (carToAdd) => {
@@ -52,12 +51,7 @@ SupermarketModel.addCar = (carToAdd) => {
 SupermarketModel.removeCar = (carName) => {
     return SupermarketModel.remove({ name: carName });
 }
-function calcCrow(lat1, lon1, lat2, lon2) {
-    //   console.log("lat1 is: " + lat1);
-    // console.log("lon1 is: " + lon1);
-    //console.log("lat2 is: " + lat2);
-    // console.log("lon2 is: " + lon2);
-
+function calcDistance(lat1, lon1, lat2, lon2) {
     var R = 6371; // Radius of the earth in km
     var dLat = deg2rad(lat2 - lat1); // deg2rad below
     var dLon = deg2rad(lon2 - lon1);
